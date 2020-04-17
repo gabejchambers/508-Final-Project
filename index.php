f197dffadb05386e317dc97fb55f966f20c85154
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-17 19:56:07 -0400

    testing php

--- index.php
+++ index.php
@@ -13,6 +13,25 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+    echo "Connected successfully\n";
     echo "what it do boy";
+
+
+    $sql = "INSERT INTO Employee 
+        VALUES ('4444', '372e25f23b5a8ae33c7ba203412ace30', '3342', 'Hugo', 3400.00, '1 Your Street')";
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+
+    $sql2 = "INSERT INTO Store VALUES ('3342', '321 North Street', '4444')";
+    if (mysqli_query($conn, $sql2)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 ?>
