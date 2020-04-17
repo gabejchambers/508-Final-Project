d461e25d79c3d085fba69b390012900b7a3e2cf0
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-17 19:13:05 -0400

    testing if mysqli works

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-    phpinfo();
+    #phpinfo();
+
+    $servername = "3.234.246.29";
+    $username = "project_15";
+    $password = "V00827834";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
 ?>
