61fb3dd2c8fbc0499c16134c365b4b7169a3c184
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-17 19:20:45 -0400

    troubleshoot

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-    #phpinfo();
+    phpinfo();
 
-    $servername = "project_15@3.234.246.29";
-    #$username = "project_15";
+    $servername = "3.234.246.29";
+    $username = "project_15";
     $password = "V00827834";
 
     // Create connection
-    #$conn = new mysqli($servername, $username, $password);
-    $conn = new mysqli($servername, $password);
+    $conn = new mysqli($servername, $username, $password);
 
     // Check connection
     if ($conn->connect_error) {
