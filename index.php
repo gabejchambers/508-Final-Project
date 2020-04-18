a510ad190019ff6856793d22cb254483eac80134
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-17 20:10:02 -0400

    php query

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully\n";
-    echo "what it do boy";
 
+
+    #Inserting data w php
 /* This worked!
     $sql = "INSERT INTO Employee 
         VALUES ('5656', '372e25f23b5a8ae33c5ba203412ace30', '1600', 'Hannah', 400.00, '12 Your Street')";
@@ -26,5 +26,21 @@
     }
 */
 
+
+    #Query w php:
+    $sql = "SELECT name, salary FROM Employee WHERE EID = 5656";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "Name: " . $row["name"]. " - Salary: " . $row["salary"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+
+    echo "Connected successfully";
     mysqli_close($conn);
 ?>
