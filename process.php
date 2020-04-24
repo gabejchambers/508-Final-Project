f92a8b939d38a4f63ffddd95a0afffdb7052dfaa
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-24 19:20:57 -0400

    testaroo

--- process.php
+++ process.php
@@ -15,37 +15,6 @@ if ($conn->connect_error) {
 }
 
 
-#Inserting data w php
-/* This worked!
-    $sql = "INSERT INTO Employee
-        VALUES ('5656', '372e25f23b5a8ae33c5ba203412ace30', '1600', 'Hannah', 400.00, '12 Your Street')";
-    if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-*/
-
-$fromform = $_POST['data'];
-if ($fromform) { #wont execute if the ish is empty
-    echo $fromform;
-    echo "<br>worked<br>";
-}
-
-#Query w php:
-$sql = "SELECT name, salary FROM Employee WHERE EID = 5656";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["name"]. " - Salary: " . $row["salary"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-
-echo "Connected successfully";
+echo "other page Connected successfully";
 mysqli_close($conn);
 ?>
