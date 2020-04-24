f520635cbb6d80d7ae9bfa0a6bf9cf65cfec3767
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-24 18:59:37 -0400

    html seperate test

--- index.php
+++ index.php
@@ -11,54 +11,8 @@
     <input type="submit" value="Submit">
 </form>
 <div>
-    <?php
-    #phpinfo();
-
-    $servername = "localhost";
-    $username = "project_15";
-    $password = "V00827834";
-    $dbname = "project_15";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
 
-    #Inserting data w php
-    /* This worked!
-        $sql = "INSERT INTO Employee
-            VALUES ('5656', '372e25f23b5a8ae33c5ba203412ace30', '1600', 'Hannah', 400.00, '12 Your Street')";
-        if (mysqli_query($conn, $sql)) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-    */
-
-    $result = $_GET['data'];
-    echo $result;
-
-    #Query w php:
-    $sql = "SELECT name, salary FROM Employee WHERE EID = 5656";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "Name: " . $row["name"]. " - Salary: " . $row["salary"]. "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
-
-
-    echo "Connected successfully";
-    mysqli_close($conn);
-    ?>
 </div>
 <div>Starting here this is static HTML content.</div>
 </body>
