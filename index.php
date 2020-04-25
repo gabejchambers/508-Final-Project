43fb8ba063eeea06a4abf563574ce35811334587
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-24 20:16:27 -0400

    refact

--- index.php
+++ index.php
@@ -102,21 +102,22 @@
 
     if (isset($_POST['id_in_same'])) {
         $idin = $_POST['id_in_same'];
-    }
 
-    #Query w php and php variable:
-    $sql = "SELECT name FROM Employee WHERE EID = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $idin);
-    $stmt->execute();
-    $result = $stmt->get_result(); // get the mysqli result
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "Name: " . $row["name"]. "<br>";
+
+        #Query w php and php variable:
+        $sql = "SELECT name FROM Employee WHERE EID = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $idin);
+        $stmt->execute();
+        $result = $stmt->get_result(); // get the mysqli result
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo "Name: " . $row["name"] . "<br>";
+            }
+        } else {
+            echo "0 results";
         }
-    } else {
-        echo "0 results";
     }
 
 
