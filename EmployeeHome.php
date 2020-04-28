c06bea93540d8ecc922a214abf57a7683a96ebec
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-27 20:41:42 -0400

    mainkg employeehome

--- EmployeeHome.php
+++ EmployeeHome.php
@@ -11,7 +11,7 @@
 <br>
 <div>
     <?php
-
+    session_start();
     $servername = "localhost";
     $db_username = "project_15";
     $db_password = "V00827834";
@@ -19,12 +19,16 @@
 
     // Create connection
     $conn = new mysqli($servername, $db_username, $db_password, $dbname);
-
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
+    if ( !isset($_POST['EID'], $_POST['e_pw']) ) {
+        // Could not get the data that should have been sent.
+        exit('Please fill both the username and password fields!');
+    } else {
+        echo "correct ish";
+    }
 
     mysqli_close($conn);
     ?>
