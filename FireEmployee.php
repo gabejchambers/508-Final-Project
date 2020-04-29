70d41d58455bb44695371a5476b715abf896f08d
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-04-29 18:46:29 -0400

    debug fire a manager

--- FireEmployee.php
+++ FireEmployee.php
@@ -63,7 +63,7 @@ if (isset($_POST['submit_fire'])) {#did they click submit?
     if($EID != $_SESSION['e_id']) {#are you trying to fire yourself?
         $ism_sql = "select * from Stores s, Employee e where e.EID = s.manager and e.EID = '".$EID."'";
         $ism_result = mysqli_query($conn, $ism_sql);
-        if(!$ism_result) {#if they are not trying to fire a manager
+        if($ism_result) {#if they are not trying to fire a manager
             $sql = "delete from Employee where EID = '" . $EID . "'";
             if (mysqli_query($conn, $sql)) {#if query is successfully run
                 echo "Removed successfully";
