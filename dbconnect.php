0bfd9b6e68c3303139eee5b5a74c3742940b0f5a
Amelia <guthriean@vcu.edu>
2020-05-01 01:41:06 -0400

    test round connect file

---
+++ dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+
+//replace this on each page with include_once <<filename>>
+$servername = "localhost";
+$db_username = "project_15";
+$db_password = "V00827834";
+$dbname = "project_15";
+
+// Create connection
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
--- index.php
+++ index.php
@@ -9,18 +9,7 @@
 <div>
     <?php
 
-    $servername = "localhost";
-    $db_username = "project_15";
-    $db_password = "V00827834";
-    $dbname = "project_15";
-
-    // Create connection
-    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    include_once 'dbconnect.php';
 
 
     echo "This is a php query with formatted output:<br>";
