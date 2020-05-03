ac64807f3e02a55937e107a880c8d6b2b19e6c69
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-05-03 14:49:01 -0400

    use trigger now when fire

--- FireEmployee.php
+++ FireEmployee.php
@@ -54,40 +54,8 @@ if (isset($_POST['submit_fire'])) {
 if (isset($_POST['submit_fire'])) {#did they click submit?
     if($EID != $_SESSION['e_id']) {#are you trying to fire yourself?
         
-        #if firing a manager:
-        $ism_sql = "select * from Store s, Employee e where e.EID = s.manager and e.EID = '".$EID."'";
-        $ism_result = mysqli_query($conn, $ism_sql);
-        if(mysqli_num_rows($ism_result)!=0) {
-            #set manager of their store to null
-            $store_manager_rows = mysqli_fetch_assoc($ism_result);
-            $man_to_null_query = "update Store set manager = null WHERE manager = '".$EID."'";
-            if (!mysqli_query($conn, $man_to_null_query)) { echo "Error: " . $sql . "<br>" . mysqli_error($conn); }
-        }
-        
-        #if firing a supervisor or someone who is supervised:
-        $iss_sql = "select * from Supervise where shift_lead = '".$EID."' or employee = '".$EID."'";
-        $iss_result = mysqli_query($conn, $iss_sql);
-        if(mysqli_num_rows($iss_result)!=0) { #if they are a supervisor
-            #delete the entries is supervisors that they occur
-            while($sup_rows = $iss_result->fetch_assoc()) {
-                $sup_delete_query = "delete from Supervise where shift_lead = '".$sup_rows['shift_lead']."' and employee = '".$sup_rows['employee']."'";
-                if (!mysqli_query($conn, $sup_delete_query)) { echo "Error: " . $sql . "<br>" . mysqli_error($conn); }
-            }
-        }
-        
-        
-        #if firing an employee who has made a transaction:
-        $isc_sql = "select * from Transaction where cashier = '".$EID."'";
-        $isc_result = mysqli_query($conn, $isc_sql);
-        if(mysqli_num_rows($isc_result)!=0) { #if they have done a transaction
-            #remove them from being cashier
-            while($sup_rows = $isc_result->fetch_assoc()) {
-                $sup_delete_query = "update Transaction set cashier = null WHERE cashier = '".$EID."'";
-                if (!mysqli_query($conn, $sup_delete_query)) { echo "Error: " . $sql . "<br>" . mysqli_error($conn); }
-            }
-        }
-        
-
+        #triggers an sql trigger which checks and handles if the employee is used
+        #as a FK in relations: Supervise, Transaction, Store. trigger name delete_employee_check
         $sql = "delete from Employee where EID = '" . $EID . "'";
         if (mysqli_query($conn, $sql)) {#if query is successfully run
             echo "Removed successfully";
