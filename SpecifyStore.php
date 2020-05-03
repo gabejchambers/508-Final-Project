940d3cd9e7d18bad356b05e514c98e272e8376f7
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-05-03 17:44:22 -0400

    refactor

--- SpecifyStore.php
+++ SpecifyStore.php
@@ -1,32 +1,18 @@
 <!DOCTYPE html>
 <html>
 <body>
+<form method="POST" action="index.php">
+    <input type="submit" value="Index">
+</form>
 <div>
-<?php
-#phpinfo();
-include_once 'dbconnect.php';
-session_start();
+    <?php
+    #phpinfo();
+    include_once 'dbconnect.php';
+    session_start();
 
-$book_b = $_POST['book_val'];
+    $book_b = $_POST['book_val'];
 
-?>
-
-    <?php if(isset($_SESSION['c_loggedin'])){
-        ?>
-        <form method="POST" action="Logout.php">
-            <input type="submit" value="Logout">
-        </form>
-        <br>
-        <form method="POST" action="CustomerHome.php">
-            <input type="submit" value="Home">
-        </form>
-        <br>
-    <?php } else { ?>
-        <form method="POST" action="index.php">
-            <input type="submit" value="Index">
-        </form>
-        <br>
-    <?php } ?>
+    ?>
 
     <h2 style="text-align:center"><strong>Must Specify Store to View!!</strong></h2>
 
@@ -34,21 +20,20 @@ $book_b = $_POST['book_val'];
     <div>
         <h4>select store location id</h4>
         <?php
-//only where book is available
-        $st_sql = "select branch_name from Store";
+        $st_sql = "select SID, branch_name from Store";
         $st_rs = $conn->prepare($st_sql);
         $st_rs->execute();
         $st_result = $st_rs->get_result();
         ?>
 
-        <form method="POST" action="BookView.php" id="branch_nameList">
+        <form method="POST" action="BookView.php" id="sidList">
             <label for="storeID">Select Store ID</label>
-            <select name="branch_name_val" id="branch_name_val" form="branch_nameList">
-            <?php
+            <select name="sid_val" id="sid_val" form="sidList">
+                <?php
                 while($row = $st_result->fetch_assoc())
                 {
-                    $s_id = $row['branch_name'];
-                    echo "<option value='" . $s_id . "'>" . $s_id . "</option>";
+                    $s_id = $row['SID'];
+                    echo "<option value='" . $s_id . "'>" . $row['branch_name'] . "</option>";
                 }
                 ?>
             </select>
@@ -66,4 +51,3 @@ $book_b = $_POST['book_val'];
 </div>
 </body>
 </html>
-
